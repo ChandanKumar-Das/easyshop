b786df8e3853800c5adff02ac366b1243e03af90
Chandan-webdv <chandankumar07@gmail.com>
2024-12-20 11:38:43 +0530

    CREATE A EASYSHOP

---
+++ src/components/layout/Footer.jsx
@@ -0,0 +1,30 @@
+import React from 'react'
+
+const Footer = () => {
+    return (
+        <footer className='relative pt-12 pb-8 bg-dark-gray text-light-gray max-h-[500px]'>
+            <div className='mb-4 text-center'>
+                <h3 className='text-sm sm:text-lg'>
+                    © Easy<span className='text-sad-yellow'>Shop</span> 2023 | Developed by @Chandan
+                </h3>
+            </div>
+
+            <div className='flex justify-center items-center gap-x-4'>
+                <a className='text-4xl text-light-gray hover:text-sad-yellow hover:animate-bounce' target='_blank' href="#">
+                    <i className='bx bxl-github' ></i>
+                </a>
+
+                <a className='text-4xl text-light-gray hover:text-sad-yellow hover:animate-bounce' target='_blank' href="#">
+                    <i className='bx bxl-linkedin-square' ></i>
+                </a>
+
+                <a className='text-4xl text-light-gray hover:text-sad-yellow hover:animate-bounce' target='_blank' href="#">
+                    <i className='bx bxs-user-circle' ></i>
+                </a>
+
+            </div>
+        </footer>
+    )
+}
+
+export default Footer
